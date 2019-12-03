1e705e9d9e4b5eca8ffa6f05c8f448f9021f4ae7
sulepol <58469943+sulepol@users.noreply.github.com>
2019-12-03 22:10:21 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,6 +4,23 @@ if [ "$(id -u)" != "0" ]; then
     exit 1
 fi
 
+# Set color of logo
+tput setaf 4
+ cat << EOF
+   _____  ___  _____   __  ____________  _   _________  ________ _      _____   __   __ 
+  / _ \ \/ / |/ / _ | /  |/  /  _/ ___/ | | / /  _/ _ \/ __/ __ \ | /| / / _ | / /  / / 
+ / // /\  /    / __ |/ /|_/ // // /__   | |/ // // // / _// /_/ / |/ |/ / __ |/ /__/ /__
+/____/ /_/_/|_/_/ |_/_/  /_/___/\___/   |___/___/____/___/\____/|__/|__/_/ |_/____/____/
+                                                                                        
+EOF
+
+# Reset color
+  tput sgr 0
+
+echo "Installing dependencies..."
+echo "=========================="
+apt update && apt -y install python3 python3-pip omxplayer ntfs-3g exfat-fuse usbmount
+
 cp -a startvideos.sh /home/pi/
 cp -a startfullscreen.sh /home/pi/
 cp -a videoloop /etc/init.d/
@@ -12,12 +29,7 @@ cp -a videoloop_starter /etc/cron.d/
 cp -a looperconfig.txt /boot/
 update-rc.d videoloop defaults
 
-echo "Use NTFS USB Key? y/n"
-read option
-if [ $option == "y" ] || [ $option == "Y" ] ; then
-    apt-get update && apt-get install ntfs-3g
-    mkdir /media/USB
-    chmod a+r /media/USB
-    echo "/dev/sda1	/media/USB	ntfs-3g	defaults,noatime,nodiratime 	0	0" >> /etc/fstab
-    echo "Done!" you will need to reboot after this
-fi
+mkdir /media/USB
+chmod a+r /media/USB
+echo "/dev/sda1	/media/USB	ntfs-3g	defaults,noatime,nodiratime 	0	0" >> /etc/fstab
+echo "Done!" you will need to reboot after this
